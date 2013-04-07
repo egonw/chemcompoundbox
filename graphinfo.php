19ce4264bc316e84c854cc8b73e21d1166f46496
Egon Willighagen <egon.willighagen@gmail.com>
2013-04-08 00:08:25 +0200

    OK, first platform set up: graph based property viewing, taking InChI as input

---
+++ graphinfo.php
@@ -0,0 +1,13 @@
+<?php
+
+$graphs = array(
+  "http://www.openlinksw.com/schemas/virtrdf#"
+);
+
+$graphInfo = array(
+  "http://www.openlinksw.com/schemas/virtrdf#" => array(
+    'http://www.openlinksw.com/schemas/virtrdf#' => 'http://localhost:8893/sparql',
+  )
+);
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
 include_once("arc2/ARC2.php");
+include_once("graphinfo.php");
+
+$inchi = $_GET["inchi"];
 
 /* configuration */ 
 $config = array(
@@ -11,19 +14,38 @@ $config = array(
 /* instantiation */
 $store = ARC2::getRemoteStore($config);
 
-$q = 'SELECT * WHERE { [] ?p [] } LIMIT 10';
+$q = <<<SPARQL
+SELECT ?graph ?p ?o WHERE {
+  GRAPH ?graph {
+    ?mol cheminf:CHEMINF_000200 [ a cheminf:CHEMINF_000113 ; cheminf:SIO_000300 ?inchi . ]
+      ?p ?o .
+  }
+} LIMIT 2
+SPARQL;
+
 $rows = $store->query($q, 'rows');
 
 echo "<html>\n";
 
 echo "$rows";
 
-foreach ($rows as $row) {
-  foreach ($row as $key => $value) {
-    echo "key: $key => $value \n";
+echo "<pre>\n";
+print_r($rows);
+echo "</pre>\n";
+
+echo "<table>\n";
+foreach ($graphs as $graph) {
+  foreach ($rows as $row) {
+    if ($row['graph'] == $graph) {
+      foreach (array_keys($row) as $key) {
+        echo "<tr>\n";
+        echo "<td>$key</td><td>" . $row[$key] . "</td>\n";
+        echo "</tr>\n";
+      }
+    }
   }
 }
-
+echo "</table>\n";
 echo "</html>\n";
 
 ?>
